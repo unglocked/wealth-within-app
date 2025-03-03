8beb5170b80c26b3fc7369b0fb4f90df3c9bb2c5
unglocked <victorsingung@gmail.com>
2025-03-03 00:14:24 -0800

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// filepath: /Users/victorung/Documents/Visual Studio Projects/Wealth Within app/wealth-within-app/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/wealth-within-app/',
   plugins: [react()],
 });
